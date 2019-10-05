e98dafdefc69722091c47cc0f82d8d2480a3f502
Randel Jiao <randeljiao@gmail.com>
2019-10-05 23:17:44 +0000

    Automatically backed up by Learn

--- ger.rb
+++ ger.rb
@@ -30,6 +30,6 @@ end
 #puts dennis_ritchies_language(programmer_hash)
 
 def adding_matz(programmer_hash)
-  programmer_hash[:yukihiro_matsumoto] = known_for: "Ruby"
+  programmer_hash[:yukihiro_matsumoto] = :known_for 
 end
 puts adding_matz(programmer_hash)
